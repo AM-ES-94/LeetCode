74e8cb622e06a765e944cc27b81f61189f5fee69
Arunaditya Mondal <136542829+AM-ES-94@users.noreply.github.com>
2024-07-15 19:18:06 +0530

    Time: 4 ms (22.26%), Space: 46.6 MB (87.03%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,59 @@
+class Solution {
+    private static void breadthFirstSearch(int n, boolean[] traversal, Queue<Integer> q, List<List<Integer>> adjL){
+        q.offer(n);
+        traversal[n] = true;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int it: adjL.get(node)){
+                if(!traversal[it]){
+                    traversal[it] = true;
+                    q.offer(it);
+                }
+            }
+        }
+        return;
+    }
+
+    private static void depthFirstSearch(int node, boolean[] traversal, List<List<Integer>> adjL){
+        traversal[node] = true;
+
+        for (int it : adjL.get(node)){
+            if (!traversal[it]) depthFirstSearch(it, traversal, adjL);
+        }
+
+        return;
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        List<List<Integer>> adjL = new ArrayList<>();
+        boolean[] traversal = new boolean[isConnected.length + 1];
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i <= isConnected.length; ++i){
+            adjL.add(new ArrayList<Integer>());
+        }
+
+        for (int i = 0; i<isConnected.length; ++i){
+            for (int j = 0; j<isConnected.length; ++j){
+                if (isConnected[i][j] == 1 && i != j) {
+                    adjL.get(i+1).add(j+1);
+                    adjL.get(j+1).add(i+1);
+                }
+            }
+        }
+
+        int citiesCount = 0;
+
+        for (int i = 1; i<=isConnected.length; ++i){
+            if (!traversal[i]){
+                ++citiesCount;
+                //depthFirstSearch(i, traversal, adjL);
+                breadthFirstSearch(i, traversal, q, adjL); 
+            }
+        }
+
+        return citiesCount;
+    }
+}

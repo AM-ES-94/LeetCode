c643b8381aac88dd2be1f86c7a985103dbd3e50d
Arunaditya Mondal <136542829+AM-ES-94@users.noreply.github.com>
2024-07-18 18:48:52 +0530

    Time: 12 ms (6.59%), Space: 44.8 MB (55.54%) - LeetHub

---
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    private static boolean checkBipartiteBFS(int start, int[] traversal, List<List<Integer>> adjL){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        traversal[start] = -1;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for (int it: adjL.get(node)){
+                if (traversal[it] == 0){
+                    q.add(it);
+                    traversal[it] = -traversal[node];
+                }
+                else if (traversal[it] != 0 && traversal[it] == traversal[node]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean checkBipartiteDFS(int[] traversal, List<List<Integer>> adjL){
+
+        return true;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        List<List<Integer>> adjL = new ArrayList<>();
+
+        for (int i = 0; i < graph.length; ++i){
+            adjL.add(new ArrayList<>());
+        }
+
+        int[] traversal = new int[graph.length];
+
+        int c = 0;
+        for (int[] it: graph){
+            for (int j = 0; j < it.length; ++j){
+                adjL.get(c).add(it[j]);
+                adjL.get(it[j]).add(c);
+            }
+            ++c;
+        }
+
+        for (int i = 0; i < traversal.length; ++i){
+            if(traversal[i] == 0) {
+                if(checkBipartiteBFS(i, traversal, adjL) == false) return false;
+                //if(checkBipartiteDFS() == false) return false;
+            }
+        }
+
+        return true;
+    }
+}

51746d79a757e287cf90db73dafb6e1dcd22e708
Arunaditya Mondal <136542829+AM-ES-94@users.noreply.github.com>
2024-07-23 22:55:07 +0530

    Time: 5 ms (66.78%), Space: 44.3 MB (73.01%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,44 @@
+class Solution {
+    private static int checkPossibleBFSKahn(int[] inEdges, Queue<Integer> q, 
+    int[] sortEdges, List<List<Integer>> adjL){
+        for (int i = 0; i < inEdges.length; ++i){
+            if (inEdges[i] == 0){
+                q.offer(i);
+            }
+        }
+
+        int index = 0;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            sortEdges[index++] = node;
+
+            for (int it : adjL.get(node)){
+                inEdges[it]--;
+                if (inEdges[it] == 0) q.offer(it);
+            }
+        }
+
+        return index;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjL = new ArrayList<>();
+        int[] inEdges = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int[] sortEdges = new int[numCourses];
+
+        for (int i = 0; i < numCourses; ++i){
+            adjL.add(new ArrayList<>());
+        }
+
+        for(int arr[] : prerequisites) {
+            adjL.get(arr[1]).add(arr[0]);
+            inEdges[arr[0]]++;
+        }
+
+        int index = checkPossibleBFSKahn(inEdges, q, sortEdges, adjL);
+
+        return index == numCourses;
+    }
+}

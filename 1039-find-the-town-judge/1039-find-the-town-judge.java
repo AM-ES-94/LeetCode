7871cd76d0ab6e6420d4087b5bd3663705778ac9
Arunaditya Mondal <136542829+AM-ES-94@users.noreply.github.com>
2025-05-25 21:54:50 +0530

    Time: 2 ms (99.93%), Space: 54 MB (53.72%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int findJudge(int n, int[][] trust) {
         if(n==1) return 1;
         List<List<Integer>> adjL = new ArrayList<>();
@@ -24,6 +24,25 @@ class Solution {
            }
               
         }
+        return -1;
+    }
+}*/
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegree = new int[n + 1];
+        int[] outdegree = new int[n + 1];
+
+        for (int[] t : trust) {
+            outdegree[t[0]]++;
+            indegree[t[1]]++;
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0)
+                return i;
+        }
+
         return -1;
     }
 }

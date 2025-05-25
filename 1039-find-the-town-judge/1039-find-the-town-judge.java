3bdb801b3498042191149aec59339fd6cce76720
Arunaditya Mondal <136542829+AM-ES-94@users.noreply.github.com>
2025-05-25 20:36:40 +0530

    Time: 8 ms (20.62%), Space: 52.5 MB (86.96%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(n==1) return 1;
+        List<List<Integer>> adjL = new ArrayList<>();
+        
+        for (int i = 0; i <= n; ++i) {
+            adjL.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < trust.length; ++i) {
+            adjL.get(trust[i][0]).add(trust[i][1]);
+        }
+
+        for(int i = 1; i <= n; ++i) {
+            boolean flag = false;
+            if(adjL.get(i).size()==0) {
+                for(int j = 1; j < adjL.size(); ++j) {
+                   if(i==j) continue;
+                   flag = adjL.get(j).contains(i);
+                   if(flag == false) break;
+                }
+                if(flag == true)
+                   return i;
+           }
+              
+        }
+        return -1;
+    }
+}
